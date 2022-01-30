7c1216d5639f3a131ba4e1322b2f811744fcf957
parkpoomd <parkpoom.d@gmail.com>
2022-01-30 17:45:09 +0700

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51JIRj3AcgiKuFoIJ4jdVWsLhrvQGrJz5T3fRvjyA7Kk9TxIgtZN4HdQ1Dvq01y7euPpB2gKnBYm3vLb7aIsS9R5S00IolhGRno';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/Cuz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
